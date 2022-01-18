3f87739cd9ba2184beaa78551587fdab02a3b5d4
OlhaNester <olga_nv@ukr.net>
2022-01-18 22:32:52 +0200

    222

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
-import { ToastContainer} from 'react-toastify';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Searchbar from './components/Searchbar/Searchbar';
+import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Searchbar from "./components/Searchbar/Searchbar";
+import "react-toastify/dist/ReactToastify.css";
 
 //API key: 20298268-ad7854859c2b2dc6e8b44e367
 
 export default class App extends Component {
   state = {
-    
-    
-    query: '',
-  }
-  handleFormSubmit = query => {
-    this.setState({query});
-}
+    query: "",
+  };
+  handleFormSubmit = (query) => {
+    this.setState({ query });
+  };
 
   // async componentDidMount() {
   //   this.setState({ loading: true });
   //   await Axios.get(`https://pixabay.com/api/?q=dog&page=1&key=20298268-ad7854859c2b2dc6e8b44e367&image_type=photo&orientation=horizontal&per_page=12`)
   //     .then(response => this.setState({ images: response.data.hits })).finally(() => this.setState({ loading: false }));
-    
-    
+
   // }
   render() {
     return (
       <div>
-        <Searchbar onSubmit={ this.handleFormSubmit}/>
-        {this.state.loading &&<h1>Загружаем</h1>
-          
-        }
-        
-         <ImageGallery query={this.state.query} />
-        <ToastContainer autoClose={3000}/>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {this.state.loading && <h1>Загружаем</h1>}
+
+        <ImageGallery query={this.state.query} />
+        <ToastContainer autoClose={3000} />
       </div>
-    )
+    );
   }
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -9,36 +9,42 @@ import Axios from "axios";
 //                 src={webformatURL}
 //                 alt={tags}
 //                 largeimageurl={largeImageURL}
-                
+
 //             />
 //         );
 //     })}
 //     </ul>
 //     )
 // };
- 
- 
+
 // export default ImageGallery;
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ImageGallery extends Component {
-    state = {
-        images: [], 
-        loading: false,
+  state = {
+    images: [],
+    loading: false,
+  };
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevProps.query !== this.props.query) {
+      this.setState({ loading: true });
+      await Axios.get(
+        `https://pixabay.com/api/?q=${this.props.query}&page=1&key=20298268-ad7854859c2b2dc6e8b44e367&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then((response) => this.setState({ images: response.data.hits }))
+        .finally(() => this.setState({ loading: false }));
+      //   console.log(response);
+      //   this.setState({ images: response.data.hits });
+      //   console.log(response.data.hits);
     }
-    async componentDidUpdate(prevProps, prevState) {
-        if (prevProps.query !== this.props.query) {
-            await Axios.get(`https://pixabay.com/api/?q=${this.props.query}&page=1&key=20298268-ad7854859c2b2dc6e8b44e367&image_type=photo&orientation=horizontal&per_page=12`)
-                .then(response => this.setState({ images: response.data.hits }))  
-            // console.log(this.state.images);
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.images && <div>{this.state.images}</div>}
-            </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <>
+        {this.state.loading && <div>Загружаем...</div>}
+        {!this.props.query && <div>Введите запрос</div>}
+      </>
+    );
+  }
 }
